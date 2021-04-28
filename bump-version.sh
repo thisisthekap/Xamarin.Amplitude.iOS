10e8ab68d43767cc14b621e84aca2957aa3bd5f6
thisisthekap <c.kapplmueller@me.com>
2021-04-28 16:22:09 +0200

    added bump-version.sh

---
+++ bump-version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "USAGE: $0 VersionToBumpTo"
+    exit 1;
+fi
+git pull
+sed -i '' "s/<Version>.*<\/Version>/<Version>$1<\/Version>/" Xamarin.Amplitude.iOS/Xamarin.Amplitude.iOS.csproj
+git commit -m "bumped version to $1"
+git push
+git pull
+git tag release-bindings-v$1
+git push origin release-bindings-v$1
